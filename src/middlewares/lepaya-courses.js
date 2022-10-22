127326cab25cf7db5dd33e902ed5186e4df3dc98
Emre Demir <demiremrece@gmail.com>
2022-10-22 13:07:15 +0200

    Added a working route and middleware for fetching a course with learners and the trainer.

--- src/middlewares/lepaya-courses.js
+++ src/middlewares/lepaya-courses.js
@@ -1,22 +1,44 @@
+const fetch = require('node-fetch');
 const Response = require('../common/response');
 
-const getById = (req, res) => {
+const getById = async (req, res) => {
   try {
     const { id } = req.params;
-    res.json(Response.success({ id }));
+
+    // Fetch the course
+    const courseResponse = await fetch(`${process.env.LEPAYA_EXTERNAL_API_URL}/api/courses/${id}`)
+      .then((response) => response.json());
+
+    if (courseResponse.error) {
+      return res.status(courseResponse.statusCode)
+        .json(Response.error(courseResponse.error, courseResponse.statusCode));
+    }
+
+    const course = { ...courseResponse };
+    delete course.trainerId;
+
+    // Fetch trainer
+    const trainer = await fetch(`${process.env.LEPAYA_EXTERNAL_API_URL}/api/trainers/${courseResponse.trainerId}`)
+      .then((response) => response.json());
+
+    course.trainer = trainer;
+
+    // Fetch learners
+    const learnersFetchPromiseArray = [];
+    courseResponse.learners.forEach((learnerId) => {
+      learnersFetchPromiseArray.push(fetch(`${process.env.LEPAYA_EXTERNAL_API_URL}/api/learners/${learnerId}`)
+        .then((response) => response.json()));
+    });
+
+    // No need to do it sync, lets just grab learners and the trainer async
+    await Promise.all(learnersFetchPromiseArray).then(((responses) => {
+      course.learners = responses;
+    }));
+
+    return res.json(Response.success(course));
   } catch (error) {
-    res.json(Response.error(error.message, 500));
+    return res.json(Response.error(error.message, 500));
   }
 };
 
-const getAll = (req, res) => {
-  try {
-    res.json(Response.success([{
-      id: 'abcd-edfg', title: 'test', learners: [], trainer: {},
-    }]));
-  } catch (error) {
-    res.json(Response.error(error.message, 500));
-  }
-};
-
-module.exports = { getById, getAll };
+module.exports = { getById };
--- src/routers/lepayaCourses.js
+++ src/routers/lepayaCourses.js
@@ -3,7 +3,6 @@ const lepayaCoursesMiddleware = require('../middlewares/lepaya-courses');
 
 const router = express.Router();
 
-router.get('/', lepayaCoursesMiddleware.getAll);
 router.get('/:id', lepayaCoursesMiddleware.getById);
 
 module.exports = router;
